ccfaa04b42c1cefd2b01cb697890c8bd02d5e8ed
Pawan Pal <pawanpal2001@gmail.com>
2024-01-27 19:52:51 +0530

    updated the hover effect in skills section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,16 +8,16 @@ const Footer = () => {
         <div className="w-full flex justify-between px-20 align-middle">
                         <ul className="text-gray-900 flex justify-between gap-20 align-middle px-2 py-2 text-2xl font-medium">
                             <li>
-                                <a href="https://www.linkedin.com/in/pawanpaldev/" target="_blank" rel="noopener noreferrer"><i class="ri-linkedin-box-line" aria-label='LinkedIn Logo'></i></a>
+                                <a href="https://www.linkedin.com/in/pawanpaldev/" target="_blank" rel="noopener noreferrer" className='shadow-md text-slate-700 transition-all delay-300 hover:shadow-lg hover:text-slate-900'><i class="ri-linkedin-box-line" aria-label='LinkedIn Logo'></i></a>
                             </li>
                             <li>
-                                <a href="https://www.instagram.com/pawan_pal_ind/" target="_blank" rel="noopener noreferrer"><i class="ri-instagram-line" aria-label='Insta Logo'></i></a>
+                                <a href="https://www.instagram.com/pawan_pal_ind/" target="_blank" rel="noopener noreferrer" className='shadow-md text-slate-700 transition-all delay-300 hover:shadow-lg hover:text-slate-900'><i class="ri-instagram-line" aria-label='Insta Logo'></i></a>
                             </li>
                             <li>
-                                <a href="https://github.com/pal-pawan" target="_blank" rel="noopener noreferrer"><i class="ri-github-line" aria-label='gitHubLogo'></i></a>
+                                <a href="https://github.com/pal-pawan" target="_blank" rel="noopener noreferrer" className='shadow-md text-slate-700 transition-all delay-300 hover:shadow-lg hover:text-slate-900'><i class="ri-github-line" aria-label='gitHubLogo'></i></a>
                             </li>
                         </ul>
-                        <p className='text-gray-900 font-medium flex justify-between align-middle px-2 py-2 text-xl'>pawanpal2001@gmail.com</p>
+                        <a href='mailto:pawanpal2001@gmail.com' className='text-slate-800 font-bold flex justify-between align-middle px-2 py-2 text-xl hover:text-slate-900'>pawanpal2001@gmail.com <i class="ri-arrow-right-up-line"> </i></a>
  </div>
     </footer>
     </>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,12 +5,12 @@ const Navbar = () => {
   return (
     <>
     <nav className="flex justify-between align-middle py-4 px-2 text-grey-300 font-bold w-full bg-slate-200">
-    <div className="font-bold text-3xl align-middle px-3"><h1 className='bg-black text-white rounded-full p-3 cursor-pointer'>PP</h1></div>
-    <div className=" flex justify-between gap-8 align-middle px-8 py-3">
-        <Link href="/Home" className='text-gray-700 transition-all duration-300 no-underline font-medium text-lg hover:text-gray-900 hover:shadow-lg rounded-lg px-3 '>Home</Link>
-        <Link href="/About" className='text-gray-700 transition-all duration-300 no-underline font-medium text-lg hover:text-gray-900 hover:shadow-lg rounded-lg px-3 '>About</Link>
-        <Link href="/Skills" className='text-gray-700 transition-all duration-300 no-underline font-medium text-lg hover:text-gray-900 hover:shadow-lg rounded-lg px-3 '>Skills</Link>
-        <a href='resume.pdf' target='_blank' className='bg-slate-900 text-white text-middle rounded-lg px-3 font-medium text-lg'>Resume</a>
+    <div className="font-bold text-3xl align-middle px-3"><h1 className='bg-black text-white rounded-full shadow p-3 cursor-pointer hover:scale-110 hover:shadow-lg transition-all delay-300 hover:text-slate-900 hover:bg-yellow-50'>PP</h1></div>
+    <div className=" flex justify-between gap-8 align-middle px-8 py-2">
+        <Link href="/Home" className='text-gray-700 transition-all duration-300 no-underline font-medium text-lg hover:text-gray-900 hover:shadow-lg hover:-translate-y-1 rounded-lg px-3 py-1 '>Home</Link>
+        <Link href="/About" className='text-gray-700 transition-all duration-300 no-underline font-medium text-lg hover:text-gray-900 hover:shadow-lg hover:-translate-y-1 rounded-lg px-3 py-1 '>About</Link>
+        <Link href="/Skills" className='text-gray-700 transition-all duration-300 no-underline font-medium text-lg hover:text-gray-900 hover:shadow-lg hover:-translate-y-1 rounded-lg px-3 py-1 '>Skills</Link>
+        <a href='resume.pdf' target='_blank' className='bg-slate-900 text-white text-middle shadow-md transition-all duration-300 rounded-lg px-3 py-2 font-medium text-lg hover:-translate-y-1 hover:shadow-lg'>Resume</a>
     </div>
     </nav>
     </>
