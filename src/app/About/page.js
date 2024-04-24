4a010aa7165940c4b065df7abb1eab1e35a6beea
Pawan Pal <pawanpal2001@gmail.com>
2024-04-25 00:55:24 +0530

    project is production ready

--- src/app/About/page.js
+++ src/app/About/page.js
@@ -11,7 +11,7 @@ const About = () => {
         <div className=" w-5/6 px-8 py-20 flex justify-center bg-gray-800 rounded-lg">
           <div className="w-full flex flex-col justify-center align-middle px-5 py-10 mx-5">
             <h3 className=' text-4xl text-orange-400'>About Me</h3>
-            <p className='font-semibold text-2xl px-10 py-5'>As a web development student at Chameli Devi Institute, I'm driven by a deep passion for front-end design. Committed to creating immersive digital experiences, I eagerly embrace evolving design trends and technologies. With a keen eye for detail and a creative mindset, I'm poised to contribute innovative solutions and embark on a fulfilling career in front-end development.</p>
+            <p className='font-semibold text-2xl px-10 py-5'>As a web development student at Chameli Devi Institute, I&apos;m driven by a deep passion for front-end design. Committed to creating immersive digital experiences, I eagerly embrace evolving design trends and technologies. With a keen eye for detail and a creative mindset, I&apos;m poised to contribute innovative solutions and embark on a fulfilling career in front-end development.</p>
           </div>
         </div>
       </div>
