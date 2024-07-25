619fc942eba03da07eaebb3f79b1aeda0d4da8f0
Pawan Pal <pawanpal2001@gmail.com>
2024-07-25 11:26:35 +0530

    experiment with toggles in readme.md file

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -7,20 +7,22 @@ const Home = () => {
   return (
     <>
       <div className='w-full h-full m-0 p-0 box-border flex flex-col'>
-      <Navbar/>
-      <div className="w-full h-5/6 flex justify-center align-middle px-5 py-20">
-        <div className=" w-5/6 px-8 py-3 flex justify-around align-middle bg-gray-800 rounded-lg">
-          <div className='w-3/5 flex flex-col align-middle justify-center px-5'>
-            <h2 className='px-5 font-semibold text-lg'>Hi there, I am</h2>
-            <h1 className='text-7xl text-wrap font-medium text-orange-500 px-5 py-3'>Pawan Pal</h1>
-            <p className=' font-semibold text-2xl text-gray-400 p-5'>&quot; A Front-End Web Developer and a Competitive Programmer passionate about creating interactive applications on the web and solving complex problems by the power of data structures and algorithms. &quot;</p>
-          </div>
-          <div className='w-2/5 shadow-white overflow-hidden px-10 py-10'>
-            <Image src="pawan_img.jpg" alt="failed to load img" className='rounded-full'/>
+        <Navbar />
+        <div className="w-full flex justify-center items-center px-5 py-20">
+          <div className="w-full lg:w-5/6 px-8 py-3 flex flex-col lg:flex-row justify-around items-center bg-gray-800 rounded-lg">
+            <div className='w-full lg:w-3/5 flex flex-col items-center lg:items-start justify-center px-5'>
+              <h2 className='px-5 font-semibold text-lg text-center lg:text-left'>Hi there, I am</h2>
+              <h1 className='text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-medium text-orange-500 px-5 py-3 text-center lg:text-left'>Pawan Pal</h1>
+              <p className='font-semibold text-lg sm:text-xl md:text-2xl text-gray-400 p-5 text-center lg:text-left'>
+                &quot; A Front-End Web Developer and a Competitive Programmer passionate about creating interactive applications on the web and solving complex problems by the power of data structures and algorithms. &quot;
+              </p>
+            </div>
+            <div className='w-full lg:w-2/5 shadow-white overflow-hidden px-10 py-10 flex justify-center'>
+              <Image src="/pawan_img.jpg" alt="failed to load img" height={10} width={10} className='rounded-full h-40 sm:w-60 sm:h-60 md:w-72 md:h-72 lg:w-80 lg:h-80' />
+            </div>
           </div>
         </div>
-      </div>
-      <Footer/>
+        <Footer />
       </div>
     </>
   )
