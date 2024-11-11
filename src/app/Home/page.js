06aa1acdf172ba6801391ff2db190c5bf29ab326
pawan <pawanpal2001@gmail.com>
2024-11-11 13:07:35 +0530

    fix- replaced ' and " with their corresponding unicode

--- src/app/Home/page.js
+++ src/app/Home/page.js
@@ -13,10 +13,11 @@ const Home = () => {
           <div className='w-3/5 flex flex-col align-middle justify-center px-5'>
             <h2 className='px-5 font-semibold text-lg'>Hi there, I am</h2>
             <h1 className='text-7xl text-wrap font-medium text-orange-500 px-5 py-3'>Pawan Pal</h1>
-            <p className=' font-semibold text-2xl text-gray-400 p-5'>"A Front-End Web Developer and a Competitive Programmer passionate about creating interactive applications on the web and solving complex problems by the power of data structures and algorithms."</p>
+            <p className=' font-semibold text-2xl text-gray-400 p-5'> &quot; A Front-End Web Developer and a Competitive Programmer passionate about creating interactive applications on the web and solving complex problems by the power of data structures and algorithms.&quot;</p>
           </div>
           <div className='w-2/5 shadow-white overflow-hidden px-10 py-10'>
-            <img src="pawan_img.jpg" alt="failed to load img" className='rounded-full'/>
+          <Image src={'/pawan_img.jpg'} alt='failed to load image' className='rounded-full' width={600} height={600}/>
+            {/* <img src="pawan_img.jpg" alt="failed to load img" className='rounded-full'/> */}
           </div>
         </div>
       </div>
